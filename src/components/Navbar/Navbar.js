80958b8ea98b6c20f55e2b2449dfef21b1d2d439
liriz002 <liriz002@fiu.edu>
2020-04-24 00:00:18 -0400

    95% responsive
    Missing minor responsive details, like common spaces/margins

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,11 +9,17 @@ const Navbar = ( props ) => {
             <div id="Navbar-Elements-Container" class="container">
                 <span id="App-Name">chirper</span>
                 <span style={{ marginLeft: 'auto' }}>
+                    <span id="Hamburger-Icon">
+                        <div class="Hamburger-Icon-Bar-1"></div>
+                        <div class="Hamburger-Icon-Bar-2"></div>
+                        <div class="Hamburger-Icon-Bar-3"></div>
+                    </span>
                     <img id="Navbar-Search-Icon" src="img/icons/dark/search.svg" />
                     <input id="Navbar-Search" type="text" placeholder="Search chirps, people, or hashtags" />
                     <NavbarIcon url="img/icons/light/notifications.svg" legend="2" />
                     <NavbarIcon url="img/icons/light/messages.svg" legend="1" />
                     <NavbarIcon url="img/icons/light/profile.svg" />
+                    <img id="Navbar-Responsive-Search-Icon" src="img/icons/light/search.svg" />
                 </span>
             </div>
         </div>
